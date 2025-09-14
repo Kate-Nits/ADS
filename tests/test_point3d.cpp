e0d37dad2930b0b1be8ca5aaf0f2f7e260abcb66
Kate-Nits <ushnitskaya.ekaterina@mail.ru>
2025-09-15 02:01:12 +0300

    implemented test_point3d

--- tests/test_point3d.cpp
+++ tests/test_point3d.cpp
@@ -3,17 +3,65 @@
 #include <gtest/gtest.h>
 #include "../lib_point3d/point3d.h"
 
-//#define EXAMPLE
-
 #define EPSILON 0.000001
+#define TRUE 1
+#define FALSE 0
 
-#ifdef EXAMPLE
-TEST(TestEasyExampleLib, can_div) {
-	// Arrange
-	int x = 10;
-	int y = 2;
+TEST(TestPoint3dLib, default_constructor) {
+    // Arrange
+    Point3D a;
 
-	// Act & Assert
-	ASSERT_NO_THROW(division(x, y));
+    // Act
+    int actual_result = FALSE;
+    if (a.get_ox() == 0 && a.get_oy() == 0 && a.get_oz() == 0) {
+        actual_result = TRUE;
+    }
+
+    // Assert
+    int expected_result = TRUE;
+    EXPECT_EQ(expected_result, actual_result); //проверяет что два переданные значения равны
 }
-#endif // EXAMPLE
+
+TEST(TestPoint3dLib, parameterized_constructor) {
+    // Arrange
+    Point3D a(5, 27, -45);
+
+    // Act
+    int actual_result = FALSE;
+    if (a.get_ox() == 5 && a.get_oy() == 27 && a.get_oz() == -45) {
+        actual_result = TRUE;
+    }
+
+    // Assert
+    int expected_result = TRUE;
+    EXPECT_EQ(expected_result, actual_result); //проверяет что два переданные значения равны
+}
+
+TEST(TestPoint3dLib, copy_constructor) {
+    // Arrange
+    Point3D a(-3, 72, 15);
+    Point3D b(a);
+
+    // Act
+    int actual_result = FALSE;
+    if (a.get_ox() == b.get_ox() && a.get_oy() == b.get_oy() && a.get_oz() == b.get_oz()) {
+        actual_result = TRUE;
+    }
+
+    // Assert
+    int expected_result = TRUE;
+    EXPECT_EQ(expected_result, actual_result); //проверяет что два переданные значения равны
+}
+
+TEST(TestPoint3dLib, can_distance_to) {
+    // Arrange
+    Point3D a(0, -3, 3);
+    Point3D b(3, 1, 3);
+
+    // Act
+    float actual_result = a.distance_to(b);
+
+    // Assert
+    float expected_result = 5.0;
+    EXPECT_NEAR(expected_result, actual_result, EPSILON); //проверяет, что два числа с плавающей точкой приблизительно равны с учетом заданной погрешности
+}
